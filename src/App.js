195253ba291502a4d3992f489d969783c83c9e58
Darius346 <62722496+Darius346@users.noreply.github.com>
2020-10-05 21:20:44 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserComponent from './components/UserComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserComponent/>
+    </div>
+  );
+}
+
+export default App;
